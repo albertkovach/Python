91c17c68c875ef49c8c8b6127f08028a156c861a
albertkovach <99149122+albertkovach@users.noreply.github.com>
2023-02-08 12:48:54 +0300

    add

--- excelsearch/excelsearch.py
+++ excelsearch/excelsearch.py
@@ -24,7 +24,8 @@ def main():
         
     scrnw = (root.winfo_screenwidth()//2) - scrnwparam
     scrnh = (root.winfo_screenheight()//2) - scrnhparam
-    root.geometry('1000x500+{}+{}'.format(scrnw, scrnh))
+    #root.geometry('755x500+{}+{}'.format(scrnw, scrnh))
+    root.geometry('715x500+{}+{}'.format(scrnw, scrnh))
         
     app = GUI(root)
     root.mainloop()
@@ -63,6 +64,11 @@ class GUI(Frame):
         Res4 = IntVar()
         Res4.set(0)
     
+        global MaxRowsSearch
+        global MaxRowsData
+        global PreviewRows
+        global PreviewColumns
+        PreviewColumns = 50
     
     
         global LblColmName1
@@ -91,6 +97,7 @@ class GUI(Frame):
         ChBxAdittCond = Checkbutton(text="Дополнительное условие сопоставления 1", background=bckgcolor, variable=AdittCond1, onvalue=1, offvalue=0, font=("Arial", 11), command=InterfaceTrigger)
         ChBxAdittCond.place(x=25, y=115)
         
+        
         global LblSearchAd1
         LblSearchAd1 = Label(text="Искомых значений:", background=bckgcolor, font=("Arial", 11))
         LblSearchAd1.place(x=40, y=145)
@@ -111,6 +118,7 @@ class GUI(Frame):
         ChBxAdittCond2 = Checkbutton(text="Дополнительное условие сопоставления 2", background=bckgcolor, variable=AdittCond2, onvalue=1, offvalue=0, font=("Arial", 11), command=InterfaceTrigger)
         ChBxAdittCond2.place(x=25, y=215)
         
+        
         global LblSearchAd2
         LblSearchAd2 = Label(text="Искомых значений:", background=bckgcolor, font=("Arial", 11))
         LblSearchAd2.place(x=40, y=245)
@@ -128,8 +136,9 @@ class GUI(Frame):
         TBxSearchDataAd2.place(x=185, y=275)
         
         global ChBxAdittCond3
-        ChBxAdittCond2 = Checkbutton(text="Дополнительное условие сопоставления 3", background=bckgcolor, variable=AdittCond3, onvalue=1, offvalue=0, font=("Arial", 11), command=InterfaceTrigger)
-        ChBxAdittCond2.place(x=25, y=315)
+        ChBxAdittCond3 = Checkbutton(text="Дополнительное условие сопоставления 3", background=bckgcolor, variable=AdittCond3, onvalue=1, offvalue=0, font=("Arial", 11), command=InterfaceTrigger)
+        ChBxAdittCond3.place(x=25, y=315)
+        
         
         global LblSearchAd3
         LblSearchAd3 = Label(text="Искомых значений:", background=bckgcolor, font=("Arial", 11))
@@ -148,89 +157,105 @@ class GUI(Frame):
         TBxSearchDataAd3.place(x=185, y=375)
         
         
-        
+        Offset = -25
         
         global LblColmName2
         LblColmName2 = Label(text="Настройка столбцов результатов", background=bckgcolor, font=("Arial", 14))
-        LblColmName2.place(x=420, y=5)
+        LblColmName2.place(x=420+Offset, y=5)
         
         global LblRes1
         LblRes1 = Label(text="Результат 1 :", background=bckgcolor, font=("Arial", 11))
-        LblRes1.place(x=420, y=45)
+        LblRes1.place(x=420+Offset, y=45)
         
         global TbxRes1
         TbxRes1 = Entry(fg="black", bg=bckgcolor, width=3, font=("Arial", 11))
-        TbxRes1.place(x=520, y=45)
+        TbxRes1.place(x=520+Offset, y=45)
         
         global LblResTarget1
         LblResTarget1 = Label(text="Скопировать в столбец:", background=bckgcolor, font=("Arial", 11))
-        LblResTarget1.place(x=420, y=75)
+        LblResTarget1.place(x=420+Offset, y=75)
         
         global TbxResTarget1
         TbxResTarget1 = Entry(fg="black", bg=bckgcolor, width=3, font=("Arial", 11))
-        TbxResTarget1.place(x=595, y=75)
+        TbxResTarget1.place(x=595+Offset, y=75)
         
         global ChBxRes2
         ChBxRes2 = Checkbutton(text="Включить результат 2", background=bckgcolor, variable=Res2, onvalue=1, offvalue=0, font=("Arial", 11), command=InterfaceTrigger)
-        ChBxRes2.place(x=430, y=115)
+        ChBxRes2.place(x=430+Offset, y=115)
         
         global LblRes2
         LblRes2 = Label(text="Результат 2 :", background=bckgcolor, font=("Arial", 11))
-        LblRes2.place(x=445, y=145)
+        LblRes2.place(x=445+Offset, y=145)
         
         global TbxRes2
         TbxRes2 = Entry(fg="black", bg=bckgcolor, width=3, font=("Arial", 11))
-        TbxRes2.place(x=545, y=145)
+        TbxRes2.place(x=545+Offset, y=145)
         
         global LblResTarget2
         LblResTarget2 = Label(text="Скопировать в столбец:", background=bckgcolor, font=("Arial", 11))
-        LblResTarget2.place(x=445, y=175)
+        LblResTarget2.place(x=445+Offset, y=175)
         
         global TbxResTarget2
         TbxResTarget2 = Entry(fg="black", bg=bckgcolor, width=3, font=("Arial", 11))
-        TbxResTarget2.place(x=620, y=175)
+        TbxResTarget2.place(x=620+Offset, y=175)
         
         global ChBxRes3
         ChBxRes3 = Checkbutton(text="Включить результат 3", background=bckgcolor, variable=Res3, onvalue=1, offvalue=0, font=("Arial", 11), command=InterfaceTrigger)
-        ChBxRes3.place(x=430, y=215)
+        ChBxRes3.place(x=430+Offset, y=215)
         
         global LblRes3
         LblRes3 = Label(text="Результат 3 :", background=bckgcolor, font=("Arial", 11))
-        LblRes3.place(x=445, y=245)
+        LblRes3.place(x=445+Offset, y=245)
         
         global TbxRes3
         TbxRes3 = Entry(fg="black", bg=bckgcolor, width=3, font=("Arial", 11))
-        TbxRes3.place(x=545, y=245)
+        TbxRes3.place(x=545+Offset, y=245)
         
         global LblResTarget3
         LblResTarget3 = Label(text="Скопировать в столбец:", background=bckgcolor, font=("Arial", 11))
-        LblResTarget3.place(x=445, y=275)
+        LblResTarget3.place(x=445+Offset, y=275)
         
         global TbxResTarget3
         TbxResTarget3 = Entry(fg="black", bg=bckgcolor, width=3, font=("Arial", 11))
-        TbxResTarget3.place(x=620, y=275)
+        TbxResTarget3.place(x=620+Offset, y=275)
         
         global ChBxRes4
         ChBxRes4 = Checkbutton(text="Включить результат 4", background=bckgcolor, variable=Res4, onvalue=1, offvalue=0, font=("Arial", 11), command=InterfaceTrigger)
-        ChBxRes4.place(x=430, y=315)
+        ChBxRes4.place(x=430+Offset, y=315)
         
         global LblRes4
         LblRes4 = Label(text="Результат 4 :", background=bckgcolor, font=("Arial", 11))
-        LblRes4.place(x=445, y=345)
+        LblRes4.place(x=445+Offset, y=345)
         
         global TbxRes4
         TbxRes4 = Entry(fg="black", bg=bckgcolor, width=3, font=("Arial", 11))
-        TbxRes4.place(x=545, y=345)
+        TbxRes4.place(x=545+Offset, y=345)
         
         global LblResTarget4
         LblResTarget4 = Label(text="Скопировать в столбец:", background=bckgcolor, font=("Arial", 11))
-        LblResTarget4.place(x=445, y=375)
+        LblResTarget4.place(x=445+Offset, y=375)
         
         global TbxResTarget4
         TbxResTarget4 = Entry(fg="black", bg=bckgcolor, width=3, font=("Arial", 11))
-        TbxResTarget4.place(x=620, y=375)
+        TbxResTarget4.place(x=620+Offset, y=375)
         
         
+        
+        #global LblSearchRange
+        #LblSearchRange = Label(text="Диапазон прогонки искомых:", background=bckgcolor, font=("Arial", 11))
+        #LblSearchRange.place(x=470, y=420)
+        
+        #global TbxSearchRange
+        #TbxSearchRange = Entry(fg="black", bg=bckgcolor, width=6, font=("Arial", 11))
+        #TbxSearchRange.place(x=675, y=420)
+        
+        #global LblSearchDataRange
+        #LblSearchDataRange = Label(text="Диапазон прогонки поиска:", background=bckgcolor, font=("Arial", 11))
+        #LblSearchDataRange.place(x=470, y=450)
+        
+        #global TbxSearchDataRange
+        #TbxSearchDataRange = Entry(fg="black", bg=bckgcolor, width=6, font=("Arial", 11))
+        #TbxSearchDataRange.place(x=675, y=450)
         
         
         
@@ -244,43 +269,50 @@ class GUI(Frame):
         InputFileEntry.place(x=20, y=450)
         
         global InputFileBtn
-        InputFileBtn = Button(text='⮌', command=SelectDir, font=("Arial", 13))
+        InputFileBtn = Button(text='⮌', command=SelectFile, font=("Arial", 13))
         InputFileBtn.place(x=275, y=450, height=23)
         
         
         global OpenPrevTableBtn
-        OpenPrevTableBtn = Button(text='Просмотр', command=PreviewWindow, font=("Arial", 13))
+        OpenPrevTableBtn = Button(text='Просмотр', command=OpenPreviewWindow, font=("Arial", 13))
         OpenPrevTableBtn.place(x=320, y=430, width=105, height=43)
         OpenPrevTableBtn.configure(state = DISABLED)
         
         
         global RunBtn
-        RunBtn = Button(text='Запуск', command=Run)
-        RunBtn.place(x=20+800, y=28+38, height=24)
+        RunBtn = Button(text='Запуск', command=Run, font=("Arial", 13, "bold"))
+        #RunBtn.place(x=640, y=345, width=90, height=50)
+        RunBtn.place(x=490, y=430, width=200, height=42)
         
         
         InterfaceTrigger()
+        
+        ChBxAdittCond.configure(state = DISABLED)
+        ChBxAdittCond2.configure(state = DISABLED)
+        ChBxAdittCond3.configure(state = DISABLED)
+        ChBxRes2.configure(state = DISABLED)
+        ChBxRes3.configure(state = DISABLED)
+        ChBxRes4.configure(state = DISABLED)
 
+def OpenPreviewWindow():
+    PreviewWindow(Path(InputFile).name)
 
-
-def PreviewWindow():
+def PreviewWindow( WindowName ):
     #PreviewWindow = Toplevel(root)
     #PreviewWindow.title("New Window 1")
     #PreviewWindow.geometry("300x300")
     
     #table = TableCanvas(t_frame, cellwidth=60, thefont=('Arial',12),rowheight=18, rowheaderwidth=30,
     #                    rowselectedcolor='yellow', editable=True)
-    
-    global InputFile
-    book = openpyxl.load_workbook(InputFile)
-    sheet = book.active
-    
-    PrevColm = 30
-    PrevRows = 20
-    
+    global MaxRowsSearch
+    global MaxRowsData
+    global PreviewRows
+    global PreviewColumns
+
     PreviewWindow=Tk()
-    PreviewWindow.title(Path(InputFile).name)
-    PreviewWindow.geometry("1000x460")
+    PreviewWindow.title(WindowName)
+    #PreviewWindow.title(Path(InputFile).name)
+    PreviewWindow.geometry("1200x550")
     
     t_frame=Frame(PreviewWindow)
     t_frame.pack(fill='both', expand=True)
@@ -291,29 +323,64 @@ def PreviewWindow():
     
     data = table.model.data
     cols = table.model.columnNames #get the current columns
-
-    for i in range(PrevColm):
+    
+    #CheckMaxRows()
+    
+    book = openpyxl.load_workbook(InputFile)
+    sheet = book.active
+    
+    for i in range(PreviewColumns):
         table.model.addColumn()
     
-    for k in range(PrevRows):
+    for k in range(PreviewRows):
         table.model.addRow()
     
+    for r in range(PreviewRows):
+        for c in range(PreviewColumns):
+            if sheet.cell(row=r+1, column=c+1).value != None:
+                table.model.setValueAt(str(sheet.cell(row=r+1, column=c+1).value),r,c)
     
-    for r in range(PrevRows):
-        for c in range(PrevColm):
-            table.model.setValueAt(sheet.cell(row=r+1, column=c+1).value,r,c)
-    
+    MaxRowsSearch = PreviewRows
+    MaxRowsData = PreviewRows
     
     book.close()
     table.show()
 
+
+def CheckMaxRows():
+    global PreviewRows
+    global PreviewColumns
+
+    book = openpyxl.load_workbook(InputFile)
+    sheet = book.active
+    
+    EmptyCellCounter = 0
+    EmptyRowCounter = 0
+    
+    for t in range(100000):
+        EmptyCellCounter = 0
+        for y in range(PreviewColumns):
+            if sheet.cell(row=t+1, column=y+1).value is None:
+                EmptyCellCounter = EmptyCellCounter + 1
+                #print('t={0}, EmptyCellCounter: {1}'.format(t, EmptyCellCounter))
+        if EmptyCellCounter == PreviewColumns:
+            EmptyRowCounter = EmptyRowCounter + 1
+            #print('EmptyCellCounter == PreviewColumns')
+            #print('EmptyRowCounter={0}'.format(EmptyRowCounter))
+        if EmptyRowCounter >= 10:
+            PreviewRows = t
+            #print(t)
+            break
+    
+    book.close()
+    
 
 
 def BtnCmd():
     print('btn pressed')
 
 
-def SelectDir():
+def SelectFile():
     global InputFile
 
     InputFile = ""
@@ -325,6 +392,9 @@ def SelectDir():
         InputFileEntry.configure(state = DISABLED)
         
         OpenPrevTableBtn.configure(state = NORMAL)
+        
+        CheckMaxRows()
+        PreviewWindow(Path(InputFile).name)
         print('IDC: InputFile : {0}'.format(InputFile))
     else:
         print('IDC: InputFile not selected')
@@ -332,19 +402,19 @@ def SelectDir():
 
 def Run():
     global InputFile
-
+    
     book = openpyxl.load_workbook(InputFile)
     sheet = book.active
 
-    MaxRowsSearch = 55000
-    MaxRowsData = 55000
+    #MaxRowsSearch = int(TbxSearchRange.get())
+    #MaxRowsData = int(TbxSearchDataRange.get())
     
     # Main data
-    ColmSearch = 1
-    ColmData = 12
+    ColmSearch = int(TBxSearch.get())
+    ColmData = int(TBxSearchData.get())
     
-    ColmResult1 = 12
-    ColmResultTarget1 = 6
+    ColmResult1 = int(TbxRes1.get())
+    ColmResultTarget1 = int(TbxResTarget1.get())
     
     ArrSearch = []
     ArrData = []
@@ -352,41 +422,38 @@ def Run():
     
     # Additional conditions
     if AdittCond1.get() == 1:
-        ColmAddSearch1 = TBxSearchAd1.get()
-        ColmAddData1 = TBxSearchDataAd1.get()
+        ColmAddSearch1 = int(TBxSearchAd1.get())
+        ColmAddData1 = int(TBxSearchDataAd1.get())
+        ArrAddSearch1 = []
+        ArrAddData1 = []
     
-    ColmAddSearch2 = 0
-    ColmAddData2 = 0
+    if AdittCond2.get() == 2:
+        ColmAddSearch2 = int(TBxSearchAd2.get())
+        ColmAddData2 = int(TBxSearchDataAd2.get())
+        ArrAddSearch2 = []
+        ArrAddData2 = []
     
-    ColmAddSearch3 = 0
-    ColmAddData3 = 0
-    
-    ArrAddSearch1 = []
-    ArrAddData1 = []
-    
-    ArrAddSearch2 = []
-    ArrAddData2 = []
-    
-    ArrAddSearch3 = []
-    ArrAddData3 = []
+    if AdittCond3.get() == 3:
+        ColmAddSearch3 = int(TBxSearchAd3.get())
+        ColmAddData3 = int(TBxSearchDataAd3.get())
+        ArrAddSearch3 = []
+        ArrAddData3 = []
     
     
     # Additional results
-    ColmResult2 = 0
-    ColmResultTarget2 = 0
+    if Res2.get() == 1:
+        ColmResult2 = int(TbxRes2.get())
+        ColmResultTarget2 = int(TbxResTarget2.get())
     
-    ColmResult3 = 0
-    ColmResultTarget3 = 0
+    if Res3.get() == 1:
+        ColmResult3 = int(TbxRes3.get())
+        ColmResultTarget3 = int(TbxResTarget3.get())
     
-    ColmResult4 = 0
-    ColmResultTarget4 = 0
-    
-    
+    if Res3.get() == 1:
+        ColmResult4 = int(TbxRes4.get())
+        ColmResultTarget4 = int(TbxResTarget4.get())
 
-    
-    
 
-    
     
     for i in range(MaxRowsSearch):
         ArrSearch.append(sheet.cell(row=i+1, column=ColmSearch).value)
@@ -405,6 +472,8 @@ def Run():
                     break
                 
     book.save(InputFile)
+    messagebox.showinfo(title=None, message='Обработка завершена !')
+    PreviewWindow('РЕЗУЛЬТ ОБРАБОТКИ :  {0}'.format(Path(InputFile).name))
 
 
 def InterfaceTrigger():
